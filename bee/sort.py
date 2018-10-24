ea03092e68740964b3e8d1377cdc37feaceef9db
gengxin <guogx@broada.com>
2018-10-24 17:14:27 +0800

    thrid commit

---
+++ bee/sort.py
@@ -0,0 +1,33 @@
+# coding = utf-8
+from ._common import execute
+
+
+def getfilename(filename):
+    '''获取文件的后缀名.doc .jpg .tgz'''
+    pass
+
+
+def sort_name(files):
+    '''以类型进行排序输出'''
+    pass
+
+
+def getfilesize(filename):
+    '''获取文件的大小'''
+    pass
+
+
+def sort_size(files):
+    '''以文件大小进行排序输出'''
+    pass
+
+
+def getfiledate(filename):
+    '''获取文件的创建日期'''
+    pass
+
+
+def sort_date(files):
+    '''以日期不同进行排序出'''
+    pass
+
