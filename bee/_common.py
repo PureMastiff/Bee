c9aa2dc313c6cae213d0856cef6baaa11ff26b94
gengxin <guogx@broada.com>
2018-10-24 09:58:09 +0800

    second commit

---
+++ bee/_common.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+
+import os
+import platform
+
+
+def os_what():
+    return platform.platform()
+
+
+def excute():
+
+
+def main():
+    name = os_what()
+    print name
+
+
+if __name__ == '__main__':
+    main()
