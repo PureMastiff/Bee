c64f5a9282b308eabfddb89bcda993a7b2a0df51
gengxin <guogx@broada.com>
2018-10-24 17:51:47 +0800

    thrid commit

--- tidy.py
+++ tidy.py
@@ -2,6 +2,7 @@
 
 import os
 import time
+from bee._common import execute
 
 
 SMALL = "<5M"
@@ -10,6 +11,23 @@ BIG = ">50M"
 filessize = {SMALL: [], MIDDLE: [], BIG: []}
 
 
+class TidyUpDownload():
+    def __init__(self):
+        self.time = None
 
+    def s_time(self):
+        s_time = time.localtime(time.time())
+        return time.asctime(s_time)
+
+    def filelist(self):
+        cmd = "ls -h"
+        print "1111"
+        r = execute(cmd)
+        return r
+
+if __name__ == '__main__':
+    t = TidyUpDownload()
+    print t.s_time()
+    t.filelist()
 
 
